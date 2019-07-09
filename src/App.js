dabdf29ba64de96c183a37dc9a83e73a59b484f9
Ralph Perdomo <ralph@prdmo.com>
2019-07-09 15:25:16 -0700

    add axios request to react app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import nanoid from 'nanoid';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   const [locationId, setLocationId] = useState(nanoid(8));
@@ -8,8 +9,10 @@ function App() {
   // setLocationId(() => nanoid(8));
 
   const formAction = e => {
+    const baseUrl = 'http://localhost:3001/create/';
     e.preventDefault();
     console.log(locationId);
+    axios.post(baseUrl + locationId).then(response => console.log(response));
   };
 
   return (
