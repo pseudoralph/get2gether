f2acb700a5bada95eb3881d0eb7655f83ab7d546
Ralph Perdomo <ralph@prdmo.com>
2019-07-09 16:04:56 -0700

    remove input box

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import React, { useState } from 'react';
 import nanoid from 'nanoid';
-import './App.css';
 import axios from 'axios';
+import './App.css';
 
 function App() {
   const [locationId, setLocationId] = useState(nanoid(8));
 
-  // setLocationId(() => nanoid(8));
-
-  const formAction = e => {
+  const pushToDb = () => {
     const baseUrl = 'http://localhost:3001/create/';
-    e.preventDefault();
-    console.log(locationId);
     axios.post(baseUrl + locationId).then(response => console.log(response));
   };
 
   return (
     <>
-      <form
-        onSubmit={event => {
-          formAction(event);
-        }}
-      >
-        <input defaultValue={locationId} />
-        <button type='submit'>submit</button>
-      </form>
+      <button onClick={() => setLocationId(nanoid(8))}>refresh</button>
+
+      {locationId}
+      <button onClick={pushToDb}>submit</button>
     </>
   );
 }
