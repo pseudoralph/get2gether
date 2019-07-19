2ff69b367a059ced7fedf02e207fc32e524c1791
Ralph Perdomo <ralph@prdmo.com>
2019-07-19 16:26:54 -0700

    add geolocation request from browser api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import nanoid from 'nanoid';
 import axios from 'axios';
-import './App.css';
+import Map from './Map';
 
 function App() {
-  const [locationId, setLocationId] = useState(nanoid(8));
+  let [locationId, setLocationId] = useState(nanoid(8));
+  let [geoLocation, setGeoLocation] = useState(null);
 
   const pushToDb = () => {
     const baseUrl = 'http://localhost:3001/create/';
     axios.post(baseUrl + locationId).then(response => console.log(response));
   };
 
+  const getLatLong = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        async (position)=> {
+          const { latitude, longitude } = await position.coords
+          setGeoLocation(geoLocation={latitude,longitude})
+        })
+    }
+  }
+
+  useEffect(getLatLong,[])
+
   return (
     <>
       <button onClick={() => setLocationId(nanoid(8))}>refresh</button>
-
       {locationId}
       <button onClick={pushToDb}>submit</button>
+
+      <Map currentPos={geoLocation || null }/>
     </>
   );
 }
