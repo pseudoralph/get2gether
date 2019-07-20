2ec5c3f33136cc9b2d09718e1a65c9523e3101ea
Ralph Perdomo <ralph@prdmo.com>
2019-07-20 13:26:20 -0700

    add button to drop pin into maps component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import nanoid from 'nanoid';
 import axios from 'axios';
 import Map from './Map';
 
+const AddLocation = ({ handleOnclick }) => {
+  return (
+    <>
+      <button onClick={handleOnclick}>Add location</button>
+    </>
+  );
+};
+
 function App() {
   let [locationId, setLocationId] = useState(nanoid(8));
-  let [geoLocation, setGeoLocation] = useState(null);
-  
+  let [currentLocation, setCurrentLocation] = useState(null);
+
+  const getCurrentLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        setCurrentLocation((currentLocation = { latitude, longitude }));
+      });
+    }
+  };
+
   const pushToDb = () => {
     const baseUrl = 'http://localhost:3001/create/';
     axios.post(baseUrl + locationId).then(response => console.log(response));
   };
-  
-  const getLatLong = () => {
 
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        async (position)=> {
-          const { latitude, longitude } = await position.coords
-          setGeoLocation(geoLocation={latitude,longitude})
-        })
-      }
-    }
-    
-  useEffect(getLatLong,[])
-  return (<>
-    <button onClick={() => setLocationId(nanoid(8))}>refresh</button>
-    {locationId}
-    <button onClick={pushToDb}>submit</button>
-    <Map currentPos={geoLocation}/>
-  </>
+  return (
+    <>
+      <button onClick={() => setLocationId(nanoid(8))}>refresh</button>
+      {locationId}
+      <button onClick={pushToDb}>submit</button>
+      <AddLocation handleOnclick={getCurrentLocation} />
+      <Map markerPos={currentLocation} />
+    </>
   );
 }
 
