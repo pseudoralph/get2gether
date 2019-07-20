ac9b3941afffcbd9a7e371b4c4e0c4a4e44406ab
Ralph Perdomo <ralph@prdmo.com>
2019-07-20 13:25:41 -0700

    change smell of window binding to useRef and allow drag behvior for pins

--- src/Map.js
+++ src/Map.js
@@ -1,27 +1,40 @@
-import React, { useEffect } from 'react';
-import L from 'leaflet'
-import 'leaflet/dist/leaflet.css'
+import React, { useEffect, useRef } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
-const MapConatiner = ({currentPos}) => {
+const MapConatiner = ({ markerPos }) => {
+  const map = useRef(null);
+  const marker = useRef(null);
 
-  useEffect(()=>{
-    window.map = L.map('mapView', {
-      center: [45.4648493,-122.6531048],
+  useEffect(() => {
+    map.current = L.map('mapView', {
+      center: [45.4648493, -122.6531048],
       zoom: 13,
       layers: [L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')]
-    })
-  },[])  
-  
-  if (currentPos) {
-    L.marker(Object.values(currentPos),{
+    });
+  }, []);
+
+  if (markerPos) {
+    marker.current = L.marker(Object.values(markerPos), {
+      draggable: true,
       icon: L.icon({
-        iconUrl: 'https://www.clker.com/cliparts/S/o/O/U/j/D/google-maps-marker-for-tnqctn.svg.hi.png',
+        iconUrl:
+          'https://www.clker.com/cliparts/S/o/O/U/j/D/google-maps-marker-for-tnqctn.svg.hi.png',
         iconSize: [25, 40]
       })
-    }).addTo(window.map)
+    })
+      .addTo(map.current)
+      .addEventListener('moveend', e => {
+        console.log(e.target.getLatLng());
+      });
   }
 
-  return (<div id='mapView' style={{height: '100vh', width: '100%', position: 'absolute' }} ></div>)
-}
+  return (
+    <div
+      id='mapView'
+      style={{ height: '100vh', width: '100%', position: 'absolute' }}
+    />
+  );
+};
 
 export default MapConatiner;
