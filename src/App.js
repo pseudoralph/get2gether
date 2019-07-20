5189300f2463566d4778862e510197067738efaf
Ralph Perdomo <ralph@prdmo.com>
2019-07-19 17:10:41 -0700

    line spacing

--- src/App.js
+++ src/App.js
@@ -6,32 +6,30 @@ import Map from './Map';
 function App() {
   let [locationId, setLocationId] = useState(nanoid(8));
   let [geoLocation, setGeoLocation] = useState(null);
-
+  
   const pushToDb = () => {
     const baseUrl = 'http://localhost:3001/create/';
     axios.post(baseUrl + locationId).then(response => console.log(response));
   };
-
+  
   const getLatLong = () => {
+
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         async (position)=> {
           const { latitude, longitude } = await position.coords
           setGeoLocation(geoLocation={latitude,longitude})
         })
+      }
     }
-  }
-
+    
   useEffect(getLatLong,[])
-
-  return (
-    <>
-      <button onClick={() => setLocationId(nanoid(8))}>refresh</button>
-      {locationId}
-      <button onClick={pushToDb}>submit</button>
-
-      <Map currentPos={geoLocation || null }/>
-    </>
+  return (<>
+    <button onClick={() => setLocationId(nanoid(8))}>refresh</button>
+    {locationId}
+    <button onClick={pushToDb}>submit</button>
+    <Map currentPos={geoLocation}/>
+  </>
   );
 }
 
